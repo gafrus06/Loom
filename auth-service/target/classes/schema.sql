8d17bfcd25e8273318844b176b91a5e83ae8af29
Ruslan <127015154+gafrus06@users.noreply.github.com>
2025-09-23 19:24:55 +0700

    Delete auth-service directory

--- auth-service/target/classes/schema.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_roles(user_id, role_id)
-VALUES (
-           (SELECT id FROM users WHERE email='admin1@example.com'),
-           (SELECT id FROM roles WHERE name='ROLE_ADMIN')
-       );
